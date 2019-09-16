42f33b76d1b23edb16bbcfbe3258375f847fbfd6
paulb <paulbarden2376@gmail.com>
2019-09-16 13:30:34 -0600

    heroku support added

--- newsite/settings.py
+++ newsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,3 +145,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
